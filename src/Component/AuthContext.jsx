c9901468a9e7586930ccbe09b04c678064167b0f
unavailable16 <101920179+unavailable16@users.noreply.github.com>
2024-03-05 09:09:23 +0700

    Update AuthContext.jsx

--- src/Component/AuthContext.jsx
+++ src/Component/AuthContext.jsx
@@ -5,32 +5,42 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState('');
+  const [uniqueId, setUniqueId] = useState('');
 
   useEffect(() => {
     // On component mount, check if authentication data exists in localStorage
+    const storedUniqueId = localStorage.getItem('unique_id');
     const storedUsername = localStorage.getItem('username');
-    if (storedUsername) {
-      setIsLoggedIn(true);
+
+    if (storedUniqueId && storedUsername) {
+      setUniqueId(storedUniqueId);
       setUsername(storedUsername);
+      setIsLoggedIn(true);
     }
   }, []);
 
-  const login = (username) => {
+  const login = (username, uniqueId) => {
     setIsLoggedIn(true);
     setUsername(username);
+    setUniqueId(uniqueId);
+
     // Save authentication data to localStorage upon login
+    localStorage.setItem('unique_id', uniqueId);
     localStorage.setItem('username', username);
   };
 
   const logout = () => {
     setIsLoggedIn(false);
     setUsername('');
+    setUniqueId('');
+
     // Remove authentication data from localStorage upon logout
+    localStorage.removeItem('unique_id');
     localStorage.removeItem('username');
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout, username }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, username, uniqueId }}>
       {children}
     </AuthContext.Provider>
   );
